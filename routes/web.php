da05b0679b72538a07eb7f15d91329254d0e69dc
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2025-05-12 21:52:19 +0530

    feat(routes): exclude default auth and settings routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::get('/', function () {
     if (Auth::check()) {
@@ -18,5 +20,11 @@ Route::get('schedule', function () {
     return Inertia::render('Schedule');
 })->middleware(['auth', 'verified'])->name('schedule');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+Route::post('register', [RegisteredUserController::class, 'store']);
+Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('store');
+Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
+
+// require __DIR__.'/settings.php';
+// require __DIR__.'/auth.php';
