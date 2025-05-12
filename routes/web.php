65562c73cf15fce900891fa1bd585c095d381258
thamindu19 <92365822+thamindu19@users.noreply.github.com>
2025-05-12 15:49:08 +0530

    feat(routes): include default auth and settings routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 
 Route::get('/', function () {
     if (Auth::check()) {
@@ -12,6 +10,7 @@ Route::get('/', function () {
     }
     return Inertia::render('Book');
 })->name('home');
+
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -19,11 +18,5 @@ Route::get('schedule', function () {
     return Inertia::render('Schedule');
 })->middleware(['auth', 'verified'])->name('schedule');
 
-Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-Route::post('register', [RegisteredUserController::class, 'store']);
-Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
-Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('store');
-Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth')->name('logout');
-
-// require __DIR__.'/settings.php';
-// require __DIR__.'/auth.php';
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
